f5b65f5aadd7e8406d403ce333ad3c0c1811222a
littleclickz <hnvanpatten@gmail.com>
2024-10-23 15:20:12 -0700

    Fix a typo

--- script.js
+++ script.js
@@ -1,52 +1,116 @@
-function getComputerChoice() {
-    let randomNumber = Math.random() * 3;
-    if (randomNumber <= 1) {
-        return "rock";
-    }
-    else if (randomNumber > 1 && randomNumber <= 2) {
-        return "paper";
-    }
-    else return "scissors";
-}
-
-var computerChoice = getComputerChoice();
-
 let rock = document.querySelector("#rock");
 rock.onclick = (event) => {
     let humanChoice = "rock";
+    function getComputerChoice() {
+        let randomNumber = Math.random() * 3;
+        if (randomNumber <= 1) {
+            return "rock";
+        }
+        else if (randomNumber > 1 && randomNumber <= 2) {
+            return "paper";
+        }
+        else return "scissors";
+    }
+    var computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    var humanScore = 0;
+    var computerScore = 0;
+    var roundResult = playRound(humanChoice,computerChoice);
+    console.log(roundResult);
+    if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
+        computerScore ++;
+    }
+    else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
+        humanScore ++;
+    }
+    console.log("Your score is: " + humanScore);
+    console.log("The final scores are " + computerScore + " points for the computer and " + humanScore + " points for you.");
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!")
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lost the game! Try again?")
+    }
+    else {
+        console.log("The game is a draw! Try again?")
+    }
 };
 
 let paper = document.querySelector("#paper");
 paper.onclick = (event) => {
     let humanChoice = "paper";
+    function getComputerChoice() {
+        let randomNumber = Math.random() * 3;
+        if (randomNumber <= 1) {
+            return "rock";
+        }
+        else if (randomNumber > 1 && randomNumber <= 2) {
+            return "paper";
+        }
+        else return "scissors";
+    }
+    var computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
+    var humanScore = 0;
+    var computerScore = 0;
+    var roundResult = playRound(humanChoice,computerChoice);
+    console.log(roundResult);
+    if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
+        computerScore ++;
+    }
+    else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
+        humanScore ++;
+    }
+    console.log("Your score is: " + humanScore);
+    console.log("The final scores are " + computerScore + " points for the computer and " + humanScore + " points for you.");
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!")
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lost the game! Try again?")
+    }
+    else {
+        console.log("The game is a draw! Try again?")
+    }
 };
 
 let scissors = document.querySelector("#scissors");
 scissors.onclick = (event) => {
     let humanChoice = "scissors";
-    playRound(humanChoice, computerChoice);
-};
-
-/* function getHumanChoice() {
-    let userInput = prompt("Choose your weapon!");
-    if (userInput.toLowerCase() === "rock") {
-        return "rock";
+    function getComputerChoice() {
+        let randomNumber = Math.random() * 3;
+        if (randomNumber <= 1) {
+            return "rock";
+        }
+        else if (randomNumber > 1 && randomNumber <= 2) {
+            return "paper";
+        }
+        else return "scissors";
     }
-    else if (userInput.toLowerCase() === "paper") {
-        return "paper";
+    var computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    var humanScore = 0;
+    var computerScore = 0;
+    var roundResult = playRound(humanChoice,computerChoice);
+    console.log(roundResult);
+    if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
+        computerScore ++;
+    }
+    else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
+        humanScore ++;
+    }
+    console.log("Your score is: " + humanScore);
+    console.log("The final scores are " + computerScore + " points for the computer and " + humanScore + " points for you.");
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!")
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lost the game! Try again?")
     }
     else {
-        return "scissors";
+        console.log("The game is a draw! Try again?")
     }
-}
-
-var humanChoice = getHumanChoice(); */
-
-var humanScore = 0;
-
-var computerScore = 0;
+};
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" && computerChoice === "rock") {
@@ -78,19 +142,6 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-var roundResult = playRound(humanChoice,computerChoice);
-
-console.log(roundResult);
-
-if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
-    computerScore ++;
-}
-else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
-    humanScore ++;
-}
-
-console.log("Your score is: " + humanScore);
-
 // everything under here makes additional rounds happen - but how do I wrap the playGame function around the above
 // stuff to clean up the code?
 
@@ -105,58 +156,4 @@ if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! 
 else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
     humanScore ++;
 }
-console.log("Your score is: " + humanScore);
-
-
-var computerChoice = getComputerChoice();
-var humanChoice = getHumanChoice();
-playRound(humanChoice, computerChoice);
-var roundResult = playRound(humanChoice,computerChoice);
-console.log(roundResult);
-if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
-    computerScore ++;
-}
-else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
-    humanScore ++;
-}
-console.log("Your score is: " + humanScore);
-
-
-var computerChoice = getComputerChoice();
-var humanChoice = getHumanChoice();
-playRound(humanChoice, computerChoice);
-var roundResult = playRound(humanChoice,computerChoice);
-console.log(roundResult);
-if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
-    computerScore ++;
-}
-else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
-    humanScore ++;
-}
-console.log("Your score is: " + humanScore);
-
-
-var computerChoice = getComputerChoice();
-var humanChoice = getHumanChoice();
-playRound(humanChoice, computerChoice);
-var roundResult = playRound(humanChoice,computerChoice);
-console.log(roundResult);
-if (roundResult === "You lose! Paper beats rock." || roundResult === "You lose! Scissors beat paper." || roundResult === "You lose! Rock beats scissors.") {
-    computerScore ++;
-}
-else if (roundResult === "You win! Paper beats rock." || roundResult === "You win! Scissors beat paper." || roundResult === "You win! Rock beats scissors.") {
-    humanScore ++;
-}
-console.log("Your score is: " + humanScore); */
-
-console.log("The final scores are " + computerScore + " points for the computer and " + humanScore + " points for you.");
-
-if (humanScore > computerScore) {
-    console.log("Congratulations! You won the game!")
-}
-else if (humanScore < computerScore) {
-    console.log("You lost the game! Try again?")
-}
-else {
-    console.log("The game is a draw! Try again?")
-}
+console.log("Your score is: " + humanScore);*/
